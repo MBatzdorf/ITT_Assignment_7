77b1b77f1ffc8c15b5b40f2a04cff4bb1790f2a0
Marco <marco@debian>
2017-06-29 15:28:50 +0200

    Added further commenting

--- analyze.py
+++ analyze.py
@@ -11,7 +11,7 @@ import wiimote_node
 
 class LogNode(Node):
     """
-    Logs the last sample from the accelerometer to stdout
+    Logs the last sample received from the accelerometer to stdout
     """
     nodeName = "Logging"
 
@@ -37,8 +37,10 @@ fclib.registerNodeType(LogNode, [('AccValues',)])
 class NormalVectorNode(Node):
     """
     Creates tuples for x-acceleration and z-acceleration values
+    with the average sensor values subtracted
     """
     nodeName = "NormalVector"
+    SIGNAL_AVERAGE = 515
 
     def __init__(self, name):
         terminals = {
@@ -50,9 +52,8 @@ class NormalVectorNode(Node):
         Node.__init__(self, name, terminals=terminals)
 
     def process(self, **kargs):
-        zVal = kargs['accelXIn'] - 521
-        xVal = kargs['accelZIn'] - 521
-        print(((0, xVal), (0, zVal)))
+        zVal = kargs['accelXIn'] - self.SIGNAL_AVERAGE
+        xVal = kargs['accelZIn'] - self.SIGNAL_AVERAGE
         return {'normalVectorX': (0, xVal), 'normalVectorZ': (0, zVal)}
 
 fclib.registerNodeType(NormalVectorNode, [('Normal',)])
@@ -67,7 +68,7 @@ def createPlotXWidget(layout, wiiNode):
     pwX = pg.PlotWidget()
     layout.addWidget(pwX, 0, 1)
     pwX.setYRange(0, 1024)
-    pwXNode = fc.createNode('PlotWidget', 'PlotWidgetX', pos=(0, -150))
+    pwXNode = fc.createNode('PlotWidget', 'PlotWidgetX')
     pwXNode.setPlot(pwX)
     bufferNodeX = fc.createNode('Buffer', 'BufferX')
     fc.connectTerminals(wiiNode['accelX'], bufferNodeX['dataIn'])
@@ -83,7 +84,7 @@ def createPlotYWidget(layout, wiiNode):
     pwY = pg.PlotWidget()
     layout.addWidget(pwY, 1, 1)
     pwY.setYRange(0, 1024)
-    pwYNode = fc.createNode('PlotWidget', 'PlotWidgetY', pos=(0, -150))
+    pwYNode = fc.createNode('PlotWidget', 'PlotWidgetY')
     pwYNode.setPlot(pwY)
     bufferNodeY = fc.createNode('Buffer', 'BufferY')
     fc.connectTerminals(wiiNode['accelY'], bufferNodeY['dataIn'])
@@ -99,7 +100,7 @@ def createPlotZWidget(layout, wiiNode):
     pwZ = pg.PlotWidget()
     layout.addWidget(pwZ, 2, 1)
     pwZ.setYRange(0, 1024)
-    pwZNode = fc.createNode('PlotWidget', 'PlotWidgetZ', pos=(0, -150))
+    pwZNode = fc.createNode('PlotWidget', 'PlotWidgetZ')
     pwZNode.setPlot(pwZ)
     bufferNodeZ = fc.createNode('Buffer', 'BufferZ')
     fc.connectTerminals(wiiNode['accelZ'], bufferNodeZ['dataIn'])
@@ -107,26 +108,26 @@ def createPlotZWidget(layout, wiiNode):
 
 
 def createNormalWidget(layout, wiiNode):
+    """ Creates a node that displays the direction the top face of the
+        wiimote is looking at on the x-z-plane
+    """
     pwN = pg.PlotWidget()
     layout.addWidget(pwN, 0, 2, 3, 1)
     pwN.setYRange(-100, 100)
-    pwNNode = fc.createNode('PlotWidget', 'PlotWidgetNormal', pos=(0, -150))
+    pwNNode = fc.createNode('PlotWidget', 'PlotWidgetNormal')
     pwNNode.setPlot(pwN)
-    normalNode = fc.createNode('NormalVector', 'NormalVector', pos=(150, 0))
+    normalNode = fc.createNode('NormalVector', 'NormalVector')
     fc.connectTerminals(wiiNode['accelX'], normalNode['accelXIn'])
     fc.connectTerminals(wiiNode['accelZ'], normalNode['accelZIn'])
-
-    curve = fc.createNode('PlotCurve', 'PlotCurve', pos=(0, -150))
-
+    curve = fc.createNode('PlotCurve', 'PlotCurve')
     fc.connectTerminals(normalNode['normalVectorZ'], curve['x'])
     fc.connectTerminals(normalNode['normalVectorX'], curve['y'])
-
     fc.connectTerminals(curve['plot'], pwNNode['In'])
 
 
 def createLogNode(wiiNode):
     """ Creates a node that outputs all acceleration events to stdout"""
-    logNode = fc.createNode('Logging', 'Logging', pos=(150, 0))
+    logNode = fc.createNode('Logging', 'Logging')
     fc.connectTerminals(wiiNode['accelX'], logNode['accelXIn'])
     fc.connectTerminals(wiiNode['accelY'], logNode['accelYIn'])
     fc.connectTerminals(wiiNode['accelZ'], logNode['accelZIn'])
@@ -149,7 +150,7 @@ if __name__ == '__main__':
 
     layout.addWidget(fc.widget(), 0, 0, 2, 1)
 
-    wiimoteNode = fc.createNode('Wiimote', 'Wiimote', pos=(0, 0))
+    wiimoteNode = fc.createNode('Wiimote', 'Wiimote')
 
     createPlotXWidget(layout, wiimoteNode)
     createPlotYWidget(layout, wiimoteNode)
